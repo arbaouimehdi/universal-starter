ac68a5cab65d99c6781cb9e6d40b56cf590272d8
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-05-16 22:25:31 +0000

    Hello World

---
+++ src/app/testimonials/testimonials.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonials',
+  template: `
+    <p>
+      testimonials works!
+    </p>
+  `,
+  styles: []
+})
+export class TestimonialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/testimonials/testimonials.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TestimonialsComponent } from './testimonials.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TestimonialsComponent]
+})
+export class TestimonialsModule { }
